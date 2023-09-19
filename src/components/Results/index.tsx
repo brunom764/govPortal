1c26d1cbef01454cd21765c869e4a762907f338b
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-09-19 14:09:19 -0300

    feat: add toFixed function

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -18,28 +18,28 @@ export default function Result({data}: {data: FormValues}){
         <Box borderWidth={1} p={32} m={2} backgroundColor="#EEEEEE">
             <Text color="#176B87" as="h2" className="text-center pb-2 text-xl">Saúde</Text>
                 <Text as="p" color="#176B87" p={2}>
-                Despesas empenhadas: <b>R$ {filteredResults[0]?.data[0].expanseCommitted || 0}</b>
+                Despesas empenhadas: <b>R$ {filteredResults[0]?.data[0].expanseCommitted.toFixed(2) || 0}</b>
                 </Text>
                 <Text as="p" color="#176B87" p={2}>
-                Despesas gastas: <b>R$ {filteredResults[0]?.data[1].expensePaid || 0}</b>
+                Despesas gastas: <b>R$ {filteredResults[0]?.data[1].expensePaid.toFixed(2) || 0}</b>
                 </Text>
             </Box>
             <Box borderWidth={1} p={32} m={2} backgroundColor="#EEEEEE">
                 <Text color="#176B87" as="h2" className="text-center pb-2 text-xl">Assistência social</Text>
                 <Text as="p" color="#176B87" p={2}>
-                    Despesas empenhadas: <b>R$ {filteredResults[0]?.data[1].expanseCommitted || 0}</b>
+                    Despesas empenhadas: <b>R$ {filteredResults[0]?.data[1].expanseCommitted.toFixed(2) || 0}</b>
                 </Text>
                 <Text as="p" color="#176B87" p={2}>
-                    Despesas gastas: <b>R$ {filteredResults[0]?.data[1].expensePaid || 0}</b>
+                    Despesas gastas: <b>R$ {filteredResults[0]?.data[1].expensePaid.toFixed(2) || 0}</b>
                     </Text>
             </Box>
             <Box borderWidth={1} p={32} m={2}  backgroundColor="#EEEEEE">
             <Text color="#176B87" as="h2" className="text-center pb-2 text-xl">Previdência social</Text>
                 <Text as="p" color="#176B87" p={2}>
-                Despesas empenhadas: <b>R$ {filteredResults[0]?.data[2].expanseCommitted || 0}</b>
+                Despesas empenhadas: <b>R$ {filteredResults[0]?.data[2].expanseCommitted.toFixed(2) || 0}</b>
                     </Text>
                 <Text as="p" color="#176B87" p={2}>
-                Despesas gastas: <b>R$ {filteredResults[0]?.data[2].expensePaid || 0}</b>
+                Despesas gastas: <b>R$ {filteredResults[0]?.data[2].expensePaid.toFixed(2) || 0}</b>
                     </Text>
             </Box>
         </Flex>
